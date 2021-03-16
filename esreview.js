b4c91e88aad1f5dba517cff0d4a3a984d1a9b7c4
Henry Doan <henrydoan96@gmail.com>
2021-03-15 20:07:58 -0600

    end of lecture

---
+++ esreview.js
@@ -0,0 +1,60 @@
+// let 
+// const
+
+// const functionName = () => {
+
+// }
+
+// functionName = () => {
+
+// }
+
+// () => {
+
+// }
+
+// const components = ({props}) => {
+  
+// }
+
+// const Index = ({props}) => {
+  
+// }
+
+// export const Show = ({props}) => {
+  
+// }
+
+// export default Index 
+
+
+// import Index, { Show } from '../Index'
+
+// Index
+
+// const person = { name: "bob", age: 23 }
+// person.name 
+// person.age
+
+// const { name, age } = person
+// const name = person.name 
+// const age = person.age 
+// name 
+// age
+
+// {...person}
+
+// promises 
+// youre asking for something and then youre getting something back in return
+
+  // .then( authStatus => { return getUser(authStatus.auth, 1) })
+  // .catch( err => console.log(err) )
+  // .finally( )
+
+  // take time to make a promise
+  // using data, it might not be defined yet 
+  // race condition 
+
+  // loader 
+  // await aysnc 
+  // display the data in other pages
